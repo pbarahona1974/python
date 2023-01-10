198cac347e0d74113eca1d5467b25ff40f44da09
pbarahona1974 <pbarahona@copaair.com>
2023-01-10 15:10:19 -0500

    pabs initial

---
+++ hola.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from markupsafe import escape
+from flask import url_for
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
+
---
+++ run.bat
@@ -0,0 +1,1 @@
+flask --app hola --debug run
